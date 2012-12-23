259e53c3a1642393fdd394ea5b5490695e039d6e
Cristian Planas <me@cristianplanas.com>
2012-12-23 15:54:22 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Love4movies::Application.config.secret_token = '73f3b60a889cb6505295142378b1508778366493378bd9f54dbbe824ebebeaa842f289da096e03a6453dbf498a2ed339960737e39ce7d653dd82b9cd5b0434db'
