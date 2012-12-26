f9c753a7d044986a8f2e7d8e2221afe0b0c4e475
Cristian Planas <me@cristianplanas.com>
2012-12-26 20:50:12 +0100

    TMDBFeeder generates movies

--- lib/TMDB_feeder.rb
+++ lib/TMDB_feeder.rb
@@ -1,7 +1,30 @@
 module TMDBFeeder
   class << self
-    def search_movies(query)
-      TMDBClient.search(query, false)
+    def generate_movies(query)
+      results = TMDBClient.search(query, false)
+      results.each do |result|
+        movie_data = TMDBClient.get_movie(result["id"])
+        movie = generate_basic_movie(movie_data)
+        movie.save
+      end
+    end
+
+    private
+
+    def generate_basic_movie(movie_data)
+      movie = Movie.new
+      [ "title", "original_title", "release_date",
+        "popularity", "revenue", "runtime", "budget",
+        "overview", "imdb_id" ].each do |attribute|
+          movie.send("#{attribute}=", movie_data["#{attribute}"])
+        end
+
+      [ "id", "vote_average", "vote_count", 
+        "poster_path" ].each do |attribute|
+        movie.send("tmdb_#{attribute}=", movie_data["#{attribute}"])
+      end
+
+      movie
     end
   end
 end
