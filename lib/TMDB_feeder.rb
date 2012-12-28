23e247ec599ac8c1ef85de154e1b2f5d8055e7f0
Cristian Planas <me@cristianplanas.com>
2012-12-28 15:10:54 +0100

    Refactoring of generate_movies method

--- lib/TMDB_feeder.rb
+++ lib/TMDB_feeder.rb
@@ -4,16 +4,15 @@ module TMDBFeeder
       results = TMDBClient.search(query, false)
       results.each do |result|
         unless Movie.find_by_tmdb_id(result["id"]) 
-          movie_data = TMDBClient.get_movie(result["id"])
-          movie = generate_basic_movie(movie_data)
-          movie.save
+          movie = generate_basic_movie(result["id"])
         end
       end
     end
 
     private
 
-    def generate_basic_movie(movie_data)
+    def generate_basic_movie(tmdb_id)
+      movie_data = TMDBClient.get_movie(tmdb_id)
       movie = Movie.new
       [ "title", "original_title", "release_date",
         "popularity", "revenue", "runtime", "budget",
@@ -26,6 +25,7 @@ module TMDBFeeder
         movie.send("tmdb_#{attribute}=", movie_data["#{attribute}"])
       end
 
+      movie.save
       movie
     end
   end
