5623056f544dce7226f6f26c6442e46723c574b2
Cristian Planas <me@cristianplanas.com>
2013-01-17 23:37:37 +0100

    Methods for calculating rating average

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,4 +13,14 @@ class Movie < ActiveRecord::Base
   has_many :technical_members, :through => :technical_participations, :source => :person
   has_and_belongs_to_many :genres
   has_many :ratings
+
+  def calculate_rating_average
+    if ratings.any?
+      total_ratings = ratings.count
+      my_ratings_average = ratings.pluck(:value).sum.to_f/total_ratings
+      (my_ratings_average * total_ratings + tmdb_vote_average * tmdb_vote_count) / total_ratings + tmdb_vote_count
+    else
+      tmdb_vote_average / tmdb_vote_count
+    end
+  end
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -6,4 +6,12 @@ class Rating < ActiveRecord::Base
 
   validates_inclusion_of :value, :in => 1..10
   validates_presence_of :movie_id, :user_id
+
+  after_create :recalculate_movie_rating
+
+  private
+
+  def recalculate_movie_rating
+    movie.update_attribute(:rating_average, movie.calculate_rating_average)
+  end
 end
