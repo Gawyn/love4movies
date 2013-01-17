5cfa51c2b795ddac12f09a884fc8ce74f474a83a
Cristian Planas <me@cristianplanas.com>
2013-01-18 00:05:00 +0100

    Creating Rating with value

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,7 +1,9 @@
 class RatingsController < ApplicationController
   def create
     Rating.create(:user_id => current_user.id,
-      :movie_id => params[:movie_id])
+      :movie_id => params[:movie_id], :value => params[:value])
+
+    @movie = Movie.find params[:movie_id]
 
     respond_to do |format|
       format.html { redirect_to :back } 
