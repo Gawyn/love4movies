1d6e7d89ac3500767665e4de44f8cc152bb06e71
Cristian Planas <me@cristianplanas.com>
2013-01-20 11:40:09 +0100

    Friendship model

---
+++ app/models/friendship.rb
@@ -0,0 +1,27 @@
+class Friendship < ActiveRecord::Base
+  attr_accessible :friend, :user
+
+  after_create :create_reciprocal_friendship
+  after_destroy :destroy_reciprocal_friendship
+
+  belongs_to :user
+  belongs_to :friend, :class_name => "User"
+
+  validate :not_myself
+  validates_uniqueness_of :user_id, :scope => :friend_id
+
+  private
+
+  def not_myself
+    errors.add(:friendship, "A user can't befriend himself") if user.id == friend.id
+  end
+
+  def create_reciprocal_friendship
+    Friendship.create(:user => friend, :friend => user) unless Friendship.find_by_user_id_and_friend_id(friend, user)
+  end
+
+  def destroy_reciprocal_friendship
+    friendship = Friendship.find_by_user_id_and_friend_id(friend, user)
+    friendship.destroy if friendship
+  end
+end
