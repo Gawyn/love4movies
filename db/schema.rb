518e72bbb941f3ba8c6751e9e2c7aeb151f08b54
Cristian Planas <me@cristianplanas.com>
2013-01-20 16:57:48 +0100

    Image subclasses

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130120154702) do
+ActiveRecord::Schema.define(:version => 20130120155159) do
 
   create_table "friendships", :force => true do |t|
     t.integer  "user_id"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(:version => 20130120154702) do
     t.string   "file_path"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
+    t.string   "type"
   end
 
   create_table "movies", :force => true do |t|
