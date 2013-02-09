6b144d1e12cc3da9cc167b1cd07ea176e0b46188
Cristian Planas <me@cristianplanas.com>
2013-02-09 23:25:50 +0100

    If list has a pattern, its title is the pattern's one

--- app/models/list.rb
+++ app/models/list.rb
@@ -8,4 +8,8 @@ class List < ActiveRecord::Base
 
   validates_presence_of :user
   validates_presence_of :title, :unless => :list_pattern
+
+  def title
+    list_pattern ? list_pattern.title : read_attribute(:title)
+  end
 end
