637852ddecd57346febe92c444eb4e46be539782
Cristian Planas <me@cristianplanas.com>
2013-02-10 01:18:42 +0100

    lists#show

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,6 +5,10 @@ class ListsController < ApplicationController
     @available_patterns = @user.available_patterns
   end
 
+  def show
+    @list = List.find params[:id]
+  end
+
   def create
     pattern = ListPattern.find(params[:list_pattern_id]) if params[:list_pattern_id]
     @list = if pattern
@@ -13,6 +17,6 @@ class ListsController < ApplicationController
       List.create(:user => current_user, :title => params[:title])
     end
 
-    redirect_to user_lists_path(:user_id => current_user)
+    redirect_to user_list_path(@list, :user_id => current_user)
   end
 end
