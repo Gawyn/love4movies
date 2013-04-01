8f3b11027a977df7efdc697fbb0c539745ff93c4
Cristian Planas <me@cristianplanas.com>
2013-04-01 21:29:22 +0200

    Refactoring ratings_count migration using pluck

--- db/migrate/20130323111947_add_ratings_count_to_user.rb
+++ db/migrate/20130323111947_add_ratings_count_to_user.rb
@@ -2,8 +2,8 @@ class AddRatingsCountToUser < ActiveRecord::Migration
   def change
     add_column :users, :ratings_count, :integer, :default => 0
 
-    User.all.each do |user|
-      User.reset_counters(user.id, :ratings)
+    User.pluck(:id).each do |user_id|
+      User.reset_counters(user_id, :ratings)
     end
   end
 end
