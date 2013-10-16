495d9383bd65ad86419b279b415b6138e77bc7a9
Cristian Planas <me@cristianplanas.com>
2013-10-16 20:16:50 +0200

    Movies are hidden by default

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131013221650) do
+ActiveRecord::Schema.define(version: 20131016181355) do
 
   create_table "comments", force: true do |t|
     t.integer  "user_id"
@@ -85,10 +85,10 @@ ActiveRecord::Schema.define(version: 20131013221650) do
     t.integer  "revenue"
     t.float    "popularity"
     t.string   "tmdb_poster_path"
-    t.datetime "created_at",                        null: false
-    t.datetime "updated_at",                        null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
     t.float    "rating_average"
-    t.boolean  "hidden",            default: false
+    t.boolean  "hidden",            default: true
     t.integer  "total_ratings",     default: 0
     t.string   "title_es"
     t.text     "overview_es"
