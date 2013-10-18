087eef612b4a5f044a0953365c9dd97dd51d41de
Cristian Planas <me@cristianplanas.com>
2013-10-18 12:31:12 +0200

    Refactoring config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,9 @@
+# RVM
+set :rvm_ruby_string, "2.0.0"
+set :rvm_type, :system
+set :rvm_install_with_sudo, true
+set :rvm_bin_path, '/usr/local/rvm/bin'
+
 set :application, "love4movies"
 set :repository,  "git@github.com:Gawyn/love4movies.git"
 set :branch, 'production'
@@ -5,10 +11,8 @@ set :rails_env, "production"
 set :deploy_to, "/home/rails/love4movies"
 set :keep_releases, 5
 set :bundle_without, [:development, :test, :test_ssl]
-
 set :scm, :git
-
-# Sidekiq
+server "love4movies.com", :app, :web, :db, :runner, primary: true
 
 # Users
 set :use_sudo, false
@@ -20,22 +24,9 @@ default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+# Callbacks
+after "deploy:update_code", "deploy:cleanup"
+after "deploy:update_code", 'deploy:symlink_config'
 
-server "love4movies.com", :app, :web, :db, :runner, primary: true
-
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+require './config/boot'
+require 'capistrano-unicorn'
