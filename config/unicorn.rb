2c2827ba0713ad14ad0b1d25996540076525e40f
Cristian Planas <me@cristianplanas.com>
2013-10-26 23:52:26 +0200

    Setting unicorn_default_pid option

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,3 +4,4 @@ unicorn_rack_env = :deployment
 app_ath = "/home/rails/love4movies/current"
 unicorn_bin = "/etc/init.d/unicorn"
 unicorn_pid = "/home/rails/love4movies/shared/pids/unicorn.pid"
+unicorn_default_pid = "/home/rails/love4movies/shared/pids/unicorn.pid"
