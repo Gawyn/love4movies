20e9a1d821422e27651c7a47329c80b348d52aaa
Cristian Planas <me@cristianplanas.com>
2013-10-27 00:03:58 +0200

    Trying another option in unicorn configuration

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,8 @@
 unicorn_env = :production
 unicorn_rack_env = :deployment
 
+pid "/home/rails/love4movies/shared/pids/unicorn.pid"
+
 app_ath = "/home/rails/love4movies/current"
 unicorn_bin = "/etc/init.d/unicorn"
 unicorn_pid = "/home/rails/love4movies/shared/pids/unicorn.pid"
