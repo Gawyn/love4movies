90a136ed330e5b9d8e910dc1a2c3df6d9e6aad95
Cristian Planas <me@cristianplanas.com>
2014-02-05 00:46:36 +0100

    Adding myself to the flow

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     if current_user
-      @followeds_activities = Activity.where(user_id: current_user.active_follows.pluck(:followed_id)).includes(:user).includes(:content).limit(12)
+      @followeds_activities = Activity.where(user_id: current_user.active_follows.pluck(:followed_id) + [current_user.id]).includes(:user).includes(:content).limit(12)
     end
   end
 end
