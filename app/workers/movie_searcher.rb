3ef81979707b3c954b3a63a07f44a9052e8a31d1
Cristian Planas <me@cristianplanas.com>
2014-02-05 00:40:01 +0100

    Search works synchronously

--- app/workers/movie_searcher.rb
+++ app/workers/movie_searcher.rb
@@ -4,7 +4,7 @@ class MovieSearcher
   def perform(query)
     TMDBClient.search(query, false).each do |result|
       tmdb_id = result["id"]
-      BackgroundSystem.enqueue(MovieGenerator, tmdb_id) unless Movie.find_by_tmdb_id(tmdb_id)
+      MovieGenerator.new.perform(tmdb_id) unless Movie.find_by_tmdb_id(tmdb_id)
     end
   end
 end
