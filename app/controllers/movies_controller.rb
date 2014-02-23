ba43585e6d28d04a1088ffeda298e9523fa371fc
Cristian Planas <me@cristianplanas.com>
2014-02-23 01:58:57 +0100

    Sending search to background again

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,8 +6,7 @@ class MoviesController < ApplicationController
   end
 
   def index
-    # BackgroundSystem.enqueue(MovieSearcher, params[:search]) if params[:search]
-    MovieSearcher.new.perform(params[:search]) if params[:search]
+    BackgroundSystem.enqueue(MovieSearcher, params[:search]) if params[:search]
     @movies = (params[:search] ? Movie.search(params[:search]) : Movie).not_hidden
   end
 
