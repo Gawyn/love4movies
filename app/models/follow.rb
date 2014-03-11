1264c3a40cc38078d65aca4321980f8ce21b4fb4
Cristian Planas <me@cristianplanas.com>
2014-03-11 23:09:51 +0100

    Validating presence of follower_id and followed_id

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -3,6 +3,7 @@ class Follow < ActiveRecord::Base
   belongs_to :followed, class_name: "User"
 
   validates_uniqueness_of :follower_id, scope: :followed_id
+  validates_presence_of :follower_id, :followed_id
   validate :follower_and_followed_are_different
 
   private
