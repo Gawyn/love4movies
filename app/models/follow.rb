89b1a79857676d114e2349989087038dae8bb01d
Cristian Planas <me@cristianplanas.com>
2014-03-11 23:08:46 +0100

    Validating that follower and followed are different in Follow

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -3,4 +3,11 @@ class Follow < ActiveRecord::Base
   belongs_to :followed, class_name: "User"
 
   validates_uniqueness_of :follower_id, scope: :followed_id
+  validate :follower_and_followed_are_different
+
+  private
+
+  def follower_and_followed_are_different
+    errors.add(:not_following_yourself, "A user can't follow himself") if follower_id == followed_id
+  end
 end
