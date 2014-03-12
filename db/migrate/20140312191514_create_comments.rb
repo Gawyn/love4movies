d23669464cd335605fe44d26065281d3f16ec563
Cristian Planas <me@cristianplanas.com>
2014-03-12 20:20:27 +0100

    Table Comment creation

--- db/migrate/20140312191514_create_comments.rb
+++ db/migrate/20140312191514_create_comments.rb
@@ -1,9 +1,10 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-      t.integer :user_id
-      t.integer :movie_id
       t.text :content
+      t.integer :user_id
+      t.string :commentable_type
+      t.integer :commentable_id
 
       t.timestamps
     end
