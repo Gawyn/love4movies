43acafac9d39ae520710490bdaf82d8f620e07d0
JosepMartins <josep@itnig.net>
2014-03-26 00:45:10 +0100

    Calc ratio for every movie poster with js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,3 +3,20 @@
 //= require turbolinks
 //= require modernizr.min
 //= require jquery.transit
+
+//Calc poster ratios
+function posterRatios() {
+  var width = $("[data-ratio]").width(),
+      height = 0;
+
+  $("[data-ratio]").each(function(i, o) {
+      height = Math.floor(Number($(o).data("ratio")) * width);
+      console.log(height);
+      $(o).height(height);
+  });
+}
+
+// Triggers
+$(document).ready(posterRatios);
+$(window).on('resize',posterRatios);
+$(document).on('page:load', posterRatios);
