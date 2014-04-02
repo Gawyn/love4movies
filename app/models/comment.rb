b09c18e13b281bd8cee471e184605d4b661f8113
Cristian Planas <cplanas@zendesk.com>
2014-04-01 21:24:13 -0700

    Not notifying when the action is by me

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -13,9 +13,9 @@ class Comment < ActiveRecord::Base
   private
 
   def notify!
-    Notification.create(user: commentable.user, notificable: self)
+    Notification.create(user: commentable.user, notificable: self) if user != commentable.user
 
     last_comment = commentable.comments.last
-    Notification.create(user: last_comment.user, notificable: last_comment) if last_comment
+    Notification.create(user: last_comment.user, notificable: last_comment) if last_comment && user != last_comment.user
   end
 end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,4 +3,12 @@ class Notification < ActiveRecord::Base
   belongs_to :user
 
   scope :pending, -> { where(pending: true) }
+
+  validates :not_to_myself
+
+  private
+
+  def not_to_myself
+    add.errors(:not_to_myself, "A user should not be notified by his own action") if user == notificable.user
+  end
 end
