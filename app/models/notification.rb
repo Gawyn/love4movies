2b6e062f5ebab132cc8e8090ac9f3664df7c1995
Cristian Planas <cplanas@zendesk.com>
2014-04-01 21:59:43 -0700

    Solving typos

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -4,11 +4,11 @@ class Notification < ActiveRecord::Base
 
   scope :pending, -> { where(pending: true) }
 
-  validates :not_to_myself
+  validate :not_to_myself
 
   private
 
   def not_to_myself
-    add.errors(:not_to_myself, "A user should not be notified by his own action") if user == notificable.user
+    errors.add(:not_to_myself, "A user should not be notified by his own action") if user == notificable.user
   end
 end
