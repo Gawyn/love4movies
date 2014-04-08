09621a4f14238d9163c0744a53181c2eb392cd78
JosepMartins <josep@itnig.net>
2014-04-08 23:29:35 +0200

    Add important activity partial for index home show

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     if current_user
-      @followeds_activities = Activity.where(user_id: current_user.followed_users_and_me_ids).includes(:user).includes(:content).order("created_at desc").limit(12)
+      @followeds_activities = Activity.where(user_id: current_user.followed_users_and_me_ids).includes(:user).includes(:content).order("created_at desc").limit(13)
     else
       @last_activity = Activity.last
     end
