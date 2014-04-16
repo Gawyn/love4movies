dcfe8c605f10b264c35f628bfa525391aaea7ab5
Gawyn <me@cristianplanas.com>
2014-04-16 07:14:30 +0200

    Repairing migration

--- db/migrate/20140415201540_add_ratings_count_to_movie.rb
+++ db/migrate/20140415201540_add_ratings_count_to_movie.rb
@@ -3,7 +3,7 @@ class AddRatingsCountToMovie < ActiveRecord::Migration
     add_column :movies, :ratings_count, :integer, default: 0
 
     Rating.pluck(:movie_id).each do |movie_id|
-      Movie.reset_counter(:ratings_count, movie_id)
+      Movie.reset_counters(:ratings_count, movie_id)
     end
   end
 end
