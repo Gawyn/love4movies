e6c7328d003049bb6708d493aaf14a9eff240914
Gawyn <me@cristianplanas.com>
2014-04-21 09:55:52 +0200

    Correcting stupid error

--- app/models/movie_in_badge.rb
+++ app/models/movie_in_badge.rb
@@ -2,6 +2,6 @@ class MovieInBadge < ActiveRecord::Base
   belongs_to :movie
   belongs_to :badge
 
-  validates_inclusion_of :movie_id, :badge_id
+  validates_presence_of :movie_id, :badge_id
   validates_uniqueness_of :movie_id, scope: :badge_id
 end
--- app/models/won_badge.rb
+++ app/models/won_badge.rb
@@ -2,6 +2,6 @@ class WonBadge < ActiveRecord::Base
   belongs_to :winner, class_name: "User"
   belongs_to :badge
 
-  validates_inclusion_of :winner_id, :badge_id
+  validates_presence_of :winner_id, :badge_id
   validates_uniqueness_of :winner_id, scope: :badge_id
 end
