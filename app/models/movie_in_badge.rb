f56eb4fbd928313056bbbb2aaf69aba38b8e387a
Gawyn <me@cristianplanas.com>
2014-04-21 09:37:11 +0200

    Adding validations to movie-badge-user associations

--- app/models/movie_in_badge.rb
+++ app/models/movie_in_badge.rb
@@ -1,4 +1,7 @@
 class MovieInBadge < ActiveRecord::Base
   belongs_to :movie
   belongs_to :badge
+
+  validates_inclusion_of :movie_id, :badge_id
+  validates_uniqueness_of :movie_id, scope: :badge_id
 end
--- app/models/won_badge.rb
+++ app/models/won_badge.rb
@@ -1,4 +1,7 @@
 class WonBadge < ActiveRecord::Base
   belongs_to :winner, class_name: "User"
   belongs_to :badge
+
+  validates_inclusion_of :winner_id, :badge_id
+  validates_uniqueness_of :winner_id, scope: :badge_id
 end
