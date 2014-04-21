458226d7fb87f5b7d1a473105f5fb156bfd163c4
Gawyn <me@cristianplanas.com>
2014-04-21 09:34:19 +0200

    Giving badges

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -5,4 +5,9 @@ class Badge < ActiveRecord::Base
   has_many :winners, through: :won_badges, source: :winner
 
   translates :name, :description
+
+  def can_receive?(user)
+    target_movie_quantity = movie_quantity || movie_in_badges.count
+    user.ratings.where(movie_id: movie_in_badges.pluck(:movie_id)).count >= target_movie_quantity
+  end
 end
