2dbc88fdac6d1e6e056d1a1b5857ee12518375a2
JosepMartins <josep@itnig.net>
2014-04-21 11:01:39 +0200

    Add new person layout

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -1,5 +1,6 @@
 class PersonsController < ApplicationController
   def show
     @person = Person.find params[:id]
+    render layout: "person"
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find params[:id]
-
     @ratings = @user.ratings.newest_first.includes(:movie).page(params[:page])
   end
 end
