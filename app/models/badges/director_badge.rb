e362061991cf1ab3b8bbaf753e3420fbcb92b42e
Gawyn <me@cristianplanas.com>
2014-04-22 22:50:23 +0200

    Marking movies for DirectorBadge

--- app/models/badges/director_badge.rb
+++ app/models/badges/director_badge.rb
@@ -1,4 +1,17 @@
 class DirectorBadge < Badge
   validates_presence_of :person_id
   belongs_to :director, class_name: "Person"
+
+  after_create :mark_movies_for_badge!
+
+  private
+
+  def mark_movies_for_badge!
+    movie_ids = TechnicalParticipation.where(person_id: person_id, job: "Director")
+      .pluck(:movie_id)
+
+    movie_ids.each do |movie_id|
+      MovieInBadge.create(badge_id: id, movie_id: movie_id)
+    end
+  end
 end
