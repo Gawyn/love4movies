0d4b0da3e1ae26c1f4540b0121923237ceab1db9
Gawyn <me@cristianplanas.com>
2014-04-27 21:19:06 +0200

    Configuring unicorn workers

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,6 +2,7 @@ unicorn_env = :production
 unicorn_rack_env = :deployment
 
 pid "/home/rails/love4movies/shared/pids/unicorn.pid"
+worker_processes 4
 
 app_ath = "/home/rails/love4movies/current"
 unicorn_bin = "/etc/init.d/unicorn"
