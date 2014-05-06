98f70875469cea6bc92595c4fad4cee75ca8fdc6
Gawyn <me@cristianplanas.com>
2014-05-06 21:46:48 +0200

    Love notifications are dependent-destroy

--- app/models/love.rb
+++ app/models/love.rb
@@ -1,6 +1,7 @@
 class Love < ActiveRecord::Base
   belongs_to :lovable, polymorphic: true
   belongs_to :user
+  has_many :notifications, as: :notificable, dependent: :destroy
 
   validates_presence_of :user_id, :lovable_id,
     :lovable_type
