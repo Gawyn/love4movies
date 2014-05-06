35450b45d1ab7dc7c943a1981a5466ab22640296
Gawyn <me@cristianplanas.com>
2014-05-06 20:16:22 +0200

    Love notifications

--- app/models/love.rb
+++ app/models/love.rb
@@ -4,4 +4,12 @@ class Love < ActiveRecord::Base
 
   validates_presence_of :user_id, :lovable_id,
     :lovable_type
+
+  after_create :notify!
+
+  private
+
+  def notify!
+    Notification.create(user: lovable.user, notificable: self, triggered_on: lovable) if user != lovable.user
+  end
 end
