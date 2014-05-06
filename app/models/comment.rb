14dfd37d17b19a7494b9d4b37c37fe42f6165c0b
Gawyn <me@cristianplanas.com>
2014-05-06 21:47:30 +0200

    Notification-Comment relation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,7 @@ class Comment < ActiveRecord::Base
 
   belongs_to :commentable, polymorphic: true
   belongs_to :user
+  has_many :notifications, as: :notificable, dependent: :destroy
 
   validates_presence_of :user_id, :content, :commentable_id,
     :commentable_type
