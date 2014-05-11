74d6269477ca76d086da0c6705fa721d159e7dfb
Gawyn <me@cristianplanas.com>
2014-05-11 14:05:46 +0200

    Users without email are valid in DB

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140507211225) do
+ActiveRecord::Schema.define(version: 20140511115925) do
 
   create_table "activities", force: true do |t|
     t.integer  "user_id"
@@ -223,7 +223,7 @@ ActiveRecord::Schema.define(version: 20140507211225) do
   add_index "reviews", ["user_id"], name: "index_reviews_on_user_id"
 
   create_table "users", force: true do |t|
-    t.string   "email",               default: "", null: false
+    t.string   "email",               default: ""
     t.string   "encrypted_password",  default: "", null: false
     t.datetime "remember_created_at"
     t.integer  "sign_in_count",       default: 0
