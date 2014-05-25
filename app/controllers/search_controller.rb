62f0f1a306abf3acb4e6462d2dab6d7e1f072abf
Gawyn <me@cristianplanas.com>
2014-05-25 15:06:57 +0200

    New search action

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
   def search
-    @type = params[:type] || "movies"
     @movies = Movie.standard_search params[:query]
     @people = Person.standard_search params[:query]
+    BackgroundSystem.enqueue(MovieSearcher, params[:query]) if params[:query]
   end
 end
