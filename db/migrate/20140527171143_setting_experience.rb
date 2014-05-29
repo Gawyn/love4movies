d602f9c6e001c92e79c576e09772bbcb9190105a
Gawyn <me@cristianplanas.com>
2014-05-29 20:24:06 +0200

    Restoring changed migration

--- db/migrate/20140527171143_setting_experience.rb
+++ db/migrate/20140527171143_setting_experience.rb
@@ -1,5 +1,7 @@
 class SettingExperience < ActiveRecord::Migration
   def change
+    add_column :users, :experience, :integer
+
     User.find_each do |user|
       user.update_attribute(:experience, user.ratings.pluck(:loves_count).compact.sum +
         (user.ratings.with_short_review.count * 2) + 
