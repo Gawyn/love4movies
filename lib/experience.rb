d8e8e6a6d103d44d669995d3b926e966b80143ec
Gawyn <me@cristianplanas.com>
2014-05-30 19:37:07 +0200

    Solving bug

--- lib/experience.rb
+++ lib/experience.rb
@@ -4,14 +4,16 @@ module Experience
   SHORT_REVIEW_EXPERIENCE = 1 + RATING_EXPERIENCE
 
   def give_experience!(user, experience_points)
+    exp = user.experience || 0
     user.reload.with_lock do
-      user.update_attribute(:experience, user.experience + experience_points)
+      user.update_attribute(:experience, exp + experience_points)
     end
   end
 
   def take_experience!(user, experience_points)
+    exp = user.experience || 0
     user.reload.with_lock do
-      user.update_attribute(:experience, user.experience - experience_points)
+      user.update_attribute(:experience, exp - experience_points)
     end
   end
 end
