4ddde179cc80ebdddb498fae8fa3b93f8c492fb8
Gawyn <me@cristianplanas.com>
2014-06-30 00:58:56 +0200

    Using SQL for recommendations

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -22,19 +22,27 @@ class MoviesController < ApplicationController
   end
 
   def recommended
-    order = params[:order] || "l4m_rating_average"
-    genre = params[:genre].blank? ? nil : params[:genre]
+    @order = params[:order] || "l4m_rating_average"
+    @genre = params[:genre].blank? ? nil : params[:genre]
+    @page = params[:page] || 1
 
-    @searched_movies = Movie.search do
-      without(:id, current_user.ratings.pluck(:movie_id))
-      order_by(order, :desc)
-      with(:genre_ids, genre) if genre
-      with(:total_ratings).greater_than(15) if order != "l4m_rating_average"
-      paginate(:page => params[:page] || 1, :per_page => Movie::RECOMMENDED_MOVIES_PER_PAGE)
+    @searched_movies = get_recommended_movies
+
+    @total_pages = (@searched_movies.count / Movie::RECOMMENDED_MOVIES_PER_PAGE).ceil
+    @movies = @searched_movies.page(@page).per(Movie::RECOMMENDED_MOVIES_PER_PAGE)
+  end
+
+  private
+
+  def get_recommended_movies
+    @movies = Movie.where("movies.id not in (?)", current_user.ratings.pluck(:movie_id))
+    @movies = @movies.joins(:genres).where("genres.id = ?", @genre) if @genre
+    @movies = @movies.more_total_ratings_than(15) if @order == "l4m_rating_average"
+
+    if @order == "rating_average"
+      @movies.by_rating_average
+    else
+      @movies.by_l4m_rating_average
     end
-
-    @total_pages = (@searched_movies.total.to_f / Movie::RECOMMENDED_MOVIES_PER_PAGE).ceil
-    @movies = @searched_movies.results
-    @order, @genre = order, genre
   end
 end
