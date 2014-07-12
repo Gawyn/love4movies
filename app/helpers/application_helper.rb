339293c9aec3eb4b88f6c2e71c79afa66315f7ed
JosepMartins <josep@itnig.net>
2014-07-12 20:43:20 +0200

    Helper for current links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,8 @@ module ApplicationHelper
   def tmdb_poster(movie)
     image_tag "http://cf2.imgobject.com/t/p/w185#{movie.tmdb_poster_path}"
   end
+
+  def cp(path)
+    "current" if current_page?(path)
+  end
 end
