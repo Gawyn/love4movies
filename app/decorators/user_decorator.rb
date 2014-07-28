9da1a4c38fb622e03f597461c95f841f00f1c738
Gawyn <me@cristianplanas.com>
2014-07-28 23:36:50 +0200

    UserDecorator full of experience related methods

---
+++ app/decorators/user_decorator.rb
@@ -0,0 +1,24 @@
+class UserDecorator < Draper::Decorator
+  delegate_all
+
+  def percentage_of_experience_achieved
+    (achieved_experience_for_next_level.to_f/Experience::EACH_LEVEL_EXPERIENCE[level - 1]).round(3)
+  end
+
+  def achieved_experience_for_next_level
+    Experience::EACH_LEVEL_EXPERIENCE[level - 1] - missing_experience_for_next_level 
+  end
+
+  def missing_experience_for_next_level
+    next_level_experience - experience
+  end
+
+  def last_level_experience
+    return 0 if level <= 1
+    Experience::SUM_LEVEL_EXPERIENCE[level - 2]
+  end
+
+  def next_level_experience
+    Experience::SUM_LEVEL_EXPERIENCE[level - 1]
+  end
+end
