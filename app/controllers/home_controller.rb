4c4fcf9be797233f0a24378b94863463649b723f
Gawyn <me@cristianplanas.com>
2014-07-29 21:59:45 +0200

    Popularity timeline

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,38 @@
 class HomeController < ApplicationController
   def index
-    @type = "activities"
     if current_user
-      @followeds_activities = Activity.where(user_id: current_user.followed_users_and_me_ids).includes(:user).includes(:content).order("created_at desc").limit(12)
-      @users = User.more_experience_first.first(10)
+      @type = calculate_type
+      show_home
     else
+      @type = "activities"
       @last_activity = Activity.last
       render layout: "landing"
+    end
+  end
+
+  def show_home
+    if @type == "activities"
+
+      @followeds_activities = Activity.where(user_id: current_user.followed_users_and_me_ids).includes(:user).includes(:content).order("created_at desc").limit(12)
+      @users = User.more_experience_first.first(10)
+
+    elsif @type == "popularity"
+      popular_movies_ids = Rating.where("created_at > ?", Time.now - 1.month).pluck(:movie_id).inject(Hash.new(0)) do |r, v|
+        r[v] += 1
+        r
+      end.sort_by(&:last).reverse[0..19].map(&:first)
+
+      @popular_movies = Movie.find(popular_movies_ids).index_by(&:id).values_at(*popular_movies_ids)
+    end
+  end
+
+  private
+
+  def calculate_type
+    if ["activities", "popularity"].include?(params[:type])
+      params[:type]
+    else
+      "activities"
     end
   end
 end
