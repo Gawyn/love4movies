b963b9a2a6d54159f9e10c4491167428d097cce1
Gawyn <me@cristianplanas.com>
2014-07-29 19:58:10 +0200

    Creating activities timeline

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @type = "activities"
     if current_user
       @followeds_activities = Activity.where(user_id: current_user.followed_users_and_me_ids).includes(:user).includes(:content).order("created_at desc").limit(12)
       @users = User.more_experience_first.first(10)
