f87d9d56f4e6751c14c69d67652ed454309acb06
JosepMartins <josep@itnig.net>
2014-07-31 22:33:25 +0200

    Fix more than scope

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -19,7 +19,7 @@ class MoviesController < ApplicationController
 
   def ranking
     @total_pages = (Movie.not_hidden.more_ratings_than(2).count / Movie::MOVIES_PER_PAGE).ceil
-    @movies = Movie.order("l4m_rating_average desc, id").not_hidden.more_ratings_than(0).page(params[:page]).per(Movie::MOVIES_PER_PAGE)
+    @movies = Movie.order("l4m_rating_average desc, id").not_hidden.more_ratings_than(2).page(params[:page]).per(Movie::MOVIES_PER_PAGE)
   end
 
   def recommended
