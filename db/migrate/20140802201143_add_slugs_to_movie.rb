feece605c739f3a5369c4ebbfbe5998962ddfca1
Gawyn <me@cristianplanas.com>
2014-08-21 22:31:12 +0200

    Not setting slugs in the migration

--- db/migrate/20140802201143_add_slugs_to_movie.rb
+++ db/migrate/20140802201143_add_slugs_to_movie.rb
@@ -1,10 +1,5 @@
 class AddSlugsToMovie < ActiveRecord::Migration
   def change
     add_column :movies, :slug, :string
-
-    Movie.find_each do |movie|
-      movie.set_slug
-      movie.save!
-    end
   end
 end
