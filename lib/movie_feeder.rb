07cdd88d2149f94d192e963ab69206cd9dec848e
Gawyn <me@cristianplanas.com>
2014-08-29 00:10:02 +0200

    Generating movies with country

--- lib/movie_feeder.rb
+++ lib/movie_feeder.rb
@@ -80,6 +80,18 @@ class MovieFeeder
         movie.genres << genre
       end
 
+      movie_data["production_countries"].each do |country_data|
+        iso = country_data["iso_3166_1"]
+        name = country_data["name"]
+        country = Country.find_by_iso_3166_1_and_name(iso, name)
+
+        if country.nil?
+          country = Country.create(iso_3166_1: iso, name: name)
+        end
+
+        movie.countries << country
+      end
+
       (LOCALES - [LOCALES.first]).each do |locale|
         movie_data = TMDBClient.get_movie(tmdb_id, locale)
         ["title", "overview"].each do |attribute|
