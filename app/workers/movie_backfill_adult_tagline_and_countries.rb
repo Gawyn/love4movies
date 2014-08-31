8e231ede0676f7f7636ea848a81fae1cad74bcef
Gawyn <me@cristianplanas.com>
2014-08-31 22:39:00 +0200

    Backfilling worker

---
+++ app/workers/movie_backfill_adult_tagline_and_countries.rb
@@ -0,0 +1,26 @@
+class MovieBackfillAdultTaglineAndCountries
+  include Sidekiq::Worker
+
+  def perform(movie_id)
+    movie = Movie.find movie_id
+    movie_data = TMDBClient.get_movie(movie.tmdb_id)
+    movie.tagline = movie_data["tagline"]
+    movie.adult = movie_data["adult"]
+
+    if movie_data["production_countries"]
+      movie_data["production_countries"].each do |country_data|
+        iso = country_data["iso_3166_1"]
+        name = country_data["name"]
+        country = Country.find_by_iso_3166_1_and_name(iso, name)
+
+        if country.nil?
+          country = Country.create(iso_3166_1: iso, name: name)
+        end
+
+        movie.countries << country
+      end
+    end
+
+    movie.save
+  end
+end
