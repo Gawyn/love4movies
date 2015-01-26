4f510d67fbe33f1f72e5638de03cdb6cf9bf7e8b
Gawyn <me@cristianplanas.com>
2015-01-26 22:02:16 +0100

    Login with password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,21 +3,18 @@ class UsersController < ApplicationController
   def login
     redirect_to root_path if current_user
 
-    omniauth = request.env["omniauth.auth"]
-    user = User.find_by_fb_uid(omniauth["uid"])
+    user = request.env["omniauth.auth"] ? omniauth_login : password_login
 
-    if user
-      user.update_data!(omniauth)
-    else
-      user = User.generate_from_omniauth(omniauth)
-    end
-
-    if user.save
+    if user && user.save
       session[:just_logged_in] = true
       sign_in_and_redirect(:user, user)
     else
       redirect_to root_path
     end
+  end
+
+  def in
+    render layout: "landing"
   end
 
   def ranking
@@ -35,5 +32,28 @@ class UsersController < ApplicationController
   def index
     @users = User.more_experience_first
     @active_follows = current_user.active_follows if current_user
+  end
+
+  private
+
+  def omniauth_login
+    omniauth = request.env["omniauth.auth"]
+    user = User.find_by_fb_uid(omniauth["uid"])
+
+    if user
+      user.update_data!(omniauth)
+    else
+      user = User.generate_from_omniauth(omniauth)
+    end
+
+    user
+  end
+
+  def password_login
+    user = User.find_by_email(params[:email])
+
+    sign_in(user) if user && user.valid_password?(params[:password])
+
+    user
   end
 end
