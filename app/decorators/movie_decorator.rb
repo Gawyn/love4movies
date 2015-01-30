9e0149d5a193859959dad8c7f33e0bd2a94081ae
Cristian Planas <cplanas@zendesk.com>
2015-01-30 15:41:08 +0100

    Priting poster as background if there is no backdrop

--- app/decorators/movie_decorator.rb
+++ app/decorators/movie_decorator.rb
@@ -6,6 +6,16 @@ class MovieDecorator < Draper::Decorator
     h.image_tag posters.first.url(Poster::SIZES.first)
   end
 
+  def get_background_image
+    if backdrop = backdrops.first
+      backdrop.url("w1280")
+    elsif poster = posters.first
+      poster.url("original")
+    else
+      ActionController::Base.helpers.asset_path("default-backdrop.jpg")
+    end
+  end
+
   def get_backdrop
     backdrops.first ? backdrops.first.url("w1280") : ActionController::Base.helpers.asset_path("default-backdrop.jpg")
   end
@@ -13,7 +23,6 @@ class MovieDecorator < Draper::Decorator
   def get_rating
     l4m_rating_average ? l4m_rating_average.round(2) : "-"
   end
-
 
   def directors
     technical_participations.where(:job => "Director").map do |participation|
