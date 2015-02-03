6ab24a1e0c0cfc02c2edd44003d08e995a5df89c
Gawyn <me@cristianplanas.com>
2015-02-03 21:17:06 +0100

    Setting a cast from string to date in migration

--- db/migrate/20150129144000_change_release_date_type_in_movie.rb
+++ db/migrate/20150129144000_change_release_date_type_in_movie.rb
@@ -1,6 +1,6 @@
 class ChangeReleaseDateTypeInMovie < ActiveRecord::Migration
   def up
-    change_column :movies, :release_date, :date
+    change_column :movies, :release_date, 'date USING CAST(release_date AS date)'
   end
 
   def down
