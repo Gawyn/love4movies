ff4ca41ee6e9ba7dbb02d19619c99043f305de17
Gawyn <me@cristianplanas.com>
2015-02-03 22:19:15 +0100

    Setting better year param

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
   end
 
   def year
+    @year = params[:year]
     @user = User.find_by_nickname params[:user_id]
     @total_pages = (@user.ratings.count.to_f / Movie::MOVIES_PER_PAGE).ceil
     @ratings = @user.ratings.by_value.joins(:movie).where("extract(year from release_date) = ?", params[:year]).page(params[:page]).per(Movie::MOVIES_PER_PAGE)
