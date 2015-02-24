2eb76b5e432e8b1c2167a4794bf63000ec751122
Gawyn <me@cristianplanas.com>
2015-02-24 22:39:18 +0100

    NotificationMailer worker

--- app/workers/notification_mailer.rb
+++ app/workers/notification_mailer.rb
@@ -2,15 +2,11 @@ class NotificationMailer
   include Sidekiq::Worker
 
   def perform(notification_id)
+    # Should be changed in the future for the user's locale.
+    I18n.locale = :en
+
     notification = Notification.find notification_id
 
-    message_params = {
-      from: "Stanley Kubrick",
-      to: notification.triggered_on.user.email,
-      subject: notification.decorate.sanitized_title,
-      text: ""
-    }
-
-    MailgunClient.send_message "love4movies.com", message_params
+    Notifier.send(:notification, notification).deliver
   end
 end
