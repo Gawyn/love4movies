eb12714aa2e7e875d5dcd72d604cd09f34d5db6c
Gawyn <me@cristianplanas.com>
2015-02-24 22:38:44 +0100

    Mailgun SMTP configuration

--- config/initializers/mailgun.rb
+++ config/initializers/mailgun.rb
@@ -1,1 +1,10 @@
-MailgunClient = Mailgun::Client.new APP_CONFIG["mailgun"]["key"]
+ActionMailer::Base.smtp_settings = {
+  :port           => 587,
+  :address        => 'smtp.mailgun.org',
+  :user_name      => APP_CONFIG["mailgun"]["username"],
+  :password       => APP_CONFIG["mailgun"]["password"],
+  :domain         => APP_CONFIG["mailgun"]["domain"],
+  :authentication => :plain,
+}
+ActionMailer::Base.delivery_method = :smtp
+Rails.application.routes.default_url_options[:host] = APP_CONFIG["mail_host"]
