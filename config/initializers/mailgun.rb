85001922cfdf83f3346364d209661714e0063091
Josep Martins <jsp.mrtns@gmail.com>
2015-04-06 18:57:00 +0200

    Responsive fixes

--- config/initializers/mailgun.rb
+++ config/initializers/mailgun.rb
@@ -1,10 +1,10 @@
-ActionMailer::Base.smtp_settings = {
-  :port           => 587,
-  :address        => 'smtp.mailgun.org',
-  :user_name      => APP_CONFIG["mailgun"]["username"],
-  :password       => APP_CONFIG["mailgun"]["password"],
-  :domain         => APP_CONFIG["mailgun"]["domain"],
-  :authentication => :plain,
-}
-ActionMailer::Base.delivery_method = :smtp
-Rails.application.routes.default_url_options[:host] = APP_CONFIG["mail_host"]
+# ActionMailer::Base.smtp_settings = {
+#   :port           => 587,
+#   :address        => 'smtp.mailgun.org',
+#   :user_name      => APP_CONFIG["mailgun"]["username"],
+#   :password       => APP_CONFIG["mailgun"]["password"],
+#   :domain         => APP_CONFIG["mailgun"]["domain"],
+#   :authentication => :plain,
+# }
+# ActionMailer::Base.delivery_method = :smtp
+# Rails.application.routes.default_url_options[:host] = APP_CONFIG["mail_host"]
