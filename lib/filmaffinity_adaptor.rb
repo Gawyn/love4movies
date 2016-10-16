13706fe550c1696bdbad764fa454f9ed5f2d7333
Gawyn <me@cristianplanas.com>
2016-10-15 22:58:54 -0700

    Bug repaired

--- lib/filmaffinity_adaptor.rb
+++ lib/filmaffinity_adaptor.rb
@@ -5,7 +5,7 @@ class FilmaffinityAdaptor
     def import_ratings(user_id, fa_user_id)
       ratings = Filmaffinity.get_user_ratings(fa_user_id)
 
-      process_ratings
+      process_ratings(user_id, ratings)
     end
 
     def process_ratings(user_id, ratings)
@@ -15,12 +15,12 @@ class FilmaffinityAdaptor
     end
 
     def process_rating(user_id, fa_rating)
-      @movies = Movie.standard_search(process_title(fa_rating["title"])).results
+      @movies = Movie.standard_search(process_title(fa_rating[:title])).results
       
       if @movies.empty?
         create_import(user_id, false, "No movies returned", nil, fa_rating)
       else
-        movie = @movies.detect { |m| m.release_date.try(:year).to_i == fa_rating["year"].to_i } || @movies.detect { |m| m.decorate.directors_names == fa_rating["director"] }
+        movie = @movies.detect { |m| m.release_date.try(:year).to_i == fa_rating[:year].to_i } || @movies.detect { |m| m.decorate.directors_names == fa_rating[:director] }
 
         if movie
           rating = Rating.find_by_user_id_and_movie_id(user_id, movie.id)
@@ -29,7 +29,7 @@ class FilmaffinityAdaptor
             rating = Rating.create(
               user_id: user_id,
               movie_id: movie.id,
-              value: fa_rating["rating"]
+              value: fa_rating[:rating]
             )
 
             msg = "Completed"
@@ -57,10 +57,10 @@ class FilmaffinityAdaptor
         status: status,
         rating_id: rating_id,
         rated_source: "Filmaffinity",
-        rated_title: fa_rating["title"],
-        rated_value: fa_rating["rating"],
-        rated_year: fa_rating["year"],
-        rated_director: fa_rating["director"]
+        rated_title: fa_rating[:title],
+        rated_value: fa_rating[:rating],
+        rated_year: fa_rating[:year],
+        rated_director: fa_rating[:director]
       )
     end
   end
