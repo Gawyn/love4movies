a0ac4604270f740f2e8e2d90c297953e89770346
Cristian Planas <gawyntrakand@gmail.com>
2019-08-15 13:23:47 -0700

    Fixing migrations

--- db/migrate/20140204235002_generating_activities.rb
+++ db/migrate/20140204235002_generating_activities.rb
@@ -1,8 +1,11 @@
 class GeneratingActivities < ActiveRecord::Migration
   def change
+    # The following migration is a data migration. It shouldn't have been here in the first place.
+=begin
     contents = (Comment.all + Rating.all).sort_by(&:updated_at).reverse
     contents.each do |content|
       Activity.create(user_id: content.user_id, content: content)
     end
+=end
   end
 end
--- db/migrate/20140311224958_rename_comment_to_review.rb
+++ db/migrate/20140311224958_rename_comment_to_review.rb
@@ -1,7 +1,12 @@
 class RenameCommentToReview < ActiveRecord::Migration
   def change
-    rename_table :comments, :reviews
-
-    Activity.where(content_type: "Comment").update_all("content_type = 'Review'")
+    # This migration changed its original purpose, now it creates the reviews table
+    create_table "reviews", force: true do |t|
+      t.integer  "user_id"
+      t.integer  "movie_id"
+      t.text     "content"
+      t.datetime "created_at"
+      t.datetime "updated_at"
+    end
   end
 end
